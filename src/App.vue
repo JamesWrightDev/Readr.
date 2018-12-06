fd40d5a0ec86688b026c483738289662d916b8ca
James <jameswrightfilm@gmail.com>
2018-12-06 09:53:27 +0000

    add modal

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,9 @@
     <v-toolbar dark color="primary">
     <v-toolbar-side-icon @click.stop="drawer = !drawer" ></v-toolbar-side-icon>
 
-    <v-toolbar-title class="white--text">Title</v-toolbar-title>
+    <v-toolbar-title class="white--text">Feed.Me</v-toolbar-title>
 
     <v-spacer></v-spacer>
-
-    <v-btn icon>
-      <v-icon>search</v-icon>
-    </v-btn>
-
-    <v-btn icon>
-      <v-icon>apps</v-icon>
-    </v-btn>
 
     <v-btn icon>
       <v-icon>refresh</v-icon>
@@ -25,7 +17,7 @@
     
   </v-toolbar>
   <!-- Add Button -->
-  <v-btn  fab dark color="primary  ">
+  <v-btn  fab dark color="primary" slot="activator" >
       <v-icon dark>add</v-icon>
     </v-btn>
 
@@ -64,6 +56,61 @@
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
+    <div class="text-xs-center">
+    <v-dialog
+      v-model="dialog"
+      width="500"
+    >
+       <v-btn  fab dark color="primary" slot="activator" >
+      <v-icon dark>add</v-icon>
+    </v-btn>
+
+      <v-card>
+        <v-card-title
+          class="headline grey lighten-2"
+          primary-title
+        >
+          Add new Feed
+        </v-card-title>
+
+
+        <v-divider></v-divider>
+         <form class='pa-5'>
+    <v-text-field
+      v-model="name"
+      :error-messages="nameErrors"
+      :counter="10"
+      label="Name"
+      required
+      @input="$v.name.$touch()"
+      @blur="$v.name.$touch()"
+    ></v-text-field>
+    <v-text-field
+      v-model="email"
+      :error-messages="emailErrors"
+      label="RSS Feed"
+      required
+      @input="$v.email.$touch()"
+      @blur="$v.email.$touch()"
+    ></v-text-field>
+    <v-select
+      v-model="select"
+      :items="items"
+      :error-messages="selectErrors"
+      label="Group"
+      required
+      @change="$v.select.$touch()"
+      @blur="$v.select.$touch()"
+    ></v-select>
+    
+
+    <v-btn @click="submit">submit</v-btn>
+    <v-btn @click="clear">clear</v-btn>
+  </form>
+        
+      </v-card>
+    </v-dialog>
+  </div>
     
   </v-app>
 </template>
